1bd4491e39f2458f3a1617e2968f8970e327fee5
Erik Laki <erik.laki@student.tuke.sk>
2023-02-27 14:33:03 +0100

    Upload New File

---
+++ PexesoBoard.java
@@ -0,0 +1,47 @@
+package Game;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PexesoBoard {
+    public final ArrayList <PexesoCard> pexesoCardList;
+    private BoardState boardState=BoardState.PLAYING;
+    private final int size;
+
+    public PexesoBoard(int size) {
+        this.size=size;
+        pexesoCardList=new ArrayList<>(size);
+        ArrayList<String> pictures = new ArrayList<>();
+        char letter='A';
+        for (int i = 0; i < size / 2; i++) {
+            pictures.add(String.valueOf(letter));
+            pictures.add(String.valueOf(letter));
+            letter++;
+        }
+        shuffle(pictures);
+        for (String picture : pictures) {
+            pexesoCardList.add(new PexesoCard(picture));
+        }
+    }
+
+    void shuffle(ArrayList pictures){
+        Collections.shuffle(pictures);
+    }
+
+    public PexesoCard getCard(int index) {
+        return pexesoCardList.get(index);
+    }
+
+    public int getSize() {
+        return pexesoCardList.size();
+    }
+
+
+    public void setBoardState(BoardState state){
+        this.boardState=state;
+    }
+
+    public BoardState getBoardState() {
+        return boardState;
+    }
+}
