8c0de0215af56611a9ca325af99a981b83f67203
Erik Laki <erik.laki@student.tuke.sk>
2023-02-27 14:33:21 +0100

    Upload New File

---
+++ Main.java
@@ -0,0 +1,9 @@
+package Game;
+
+public class Main {
+    public static void main(String[] args) {
+        PexesoBoard board=new PexesoBoard(10);
+        PexesoGameUI game=new PexesoGameUI(board);
+        game.play();
+    }
+}
